6789bbd2d386bb9d7fb7574fa0717e2b852f6821
yogendra <yogendra.raikwar.9@gmail.com>
2019-12-04 17:16:29 +0530

    adding session factory example

--- src/main/java/com/yogendra/controller/TestController.java
+++ src/main/java/com/yogendra/controller/TestController.java
@@ -2,9 +2,11 @@ package com.yogendra.controller;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +16,14 @@ public class TestController {
 	@Autowired
 	@Qualifier("jdbc1")
 	JdbcTemplate jdbcTemplate1;
-	
+
 	@Autowired
 	@Qualifier("jdbc2")
 	JdbcTemplate jdbcTemplate2;
+
+	@Autowired
+	@Qualifier("session2")
+	SessionFactory sessionFactory;
 
 	@GetMapping(value = "/test1")
 	public void test1() {
@@ -25,11 +31,12 @@ public class TestController {
 
 		System.out.println("------------ citties ------------ " + cities);
 	}
-	
-	@GetMapping(value = "/test2")
-	public void test2() {
-		List<String> cities = jdbcTemplate2.queryForList("select city from address", String.class);
 
+	@GetMapping(value = "/test2")
+	@Transactional("tx2")
+	public void test2() {
+		List<String> cities = sessionFactory.getCurrentSession().createSQLQuery("select city from address")
+				.list();
 		System.out.println("------------ citties ------------ " + cities);
 	}
 }
